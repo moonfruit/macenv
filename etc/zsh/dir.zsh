c628ee2f8407d1d985aad5e57ab2ac9740ec61de
MoonFruit <dkmoonfruit@gmail.com>
2014-07-13 09:24:52 +0800

    init add gpp

--- etc/zsh/dir.zsh
+++ etc/zsh/dir.zsh
@@ -3,16 +3,3 @@ export DOWNLOAD="$HOME/Downloads"
 export ENV="$HOME/Documents/Enviroment"
 
 export WORKSPACE="$HOME/Documents/Workspace"
-
-dev() {
-    if [ -z "$APP_HOME" ]; then
-        export APP_HOME="$WORKSPACE/gpp"
-        export NODE_ID="YY"
-
-        export TCLIHOMEDIR="/dev/null"
-        export TUXDIR="/dev/null"
-
-        source "$APP_HOME/etc/bashrc"
-    fi
-    cd "$APP_HOME"
-}
---
+++ etc/zsh/gpp.zsh
@@ -0,0 +1,7 @@
+export APP_HOME="$WORKSPACE/gpp"
+export NODE_ID="YY"
+
+export TCLIHOMEDIR="/dev/null"
+export TUXDIR="/dev/null"
+
+source "$APP_HOME/etc/bashrc"
