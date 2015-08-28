82c6e2dc2ac723956c367232d94d6573991202d6
MoonFruit <dkmoonfruit@gmail.com>
2015-08-28 11:53:49 +0800

    bug fix

--- etc/zsh/plugins/yy/yy.plugin.zsh
+++ etc/zsh/plugins/yy/yy.plugin.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/zsh
 
+# git
 alias gau='git add -u'
 compdef _git gau=git-add
+
+# autojump
+[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && source ~/.autojump/etc/profile.d/autojump.sh
+autoload -U compinit && compinit -u
