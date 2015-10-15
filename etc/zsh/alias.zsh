1fdcf873ee46a99720df8c78cd4b0b24a8ac20ae
MoonFruit <dkmoonfruit@gmail.com>
2015-10-15 10:18:06 +0800

    bug fix

--- etc/zsh/alias.zsh
+++ etc/zsh/alias.zsh
@@ -27,10 +27,10 @@ alias LS='sl'
 alias ls-='sl'
 
 vman() {
-    PAGER="/bin/sh -c \"unset PAGER;col -b -x | \
-           vim -R -c 'set ft=man nomod nolist' -c 'map q :q<CR>' \
-           -c 'map <SPACE> <C-D>' -c 'map b <C-U>' \
-           -c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' -\"" man $*
+    env PAGER="/bin/sh -c \"unset PAGER;col -b -x | \
+vim -R -c 'set ft=man nomod nolist' -c 'map q :q<CR>' \
+-c 'map <SPACE> <C-D>' -c 'map b <C-U>' \
+-c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' -\"" man $*
 }
 
 export TODOTXT_DEFAULT_ACTION=ls
--- etc/zsh/zlast.zsh
+++ etc/zsh/zlast.zsh
@@ -8,13 +8,5 @@ source =(env | grep _HOME | sed 's/\(.*\)=.*/hash -d \1="$\1"/')
 # for PATH
 export PATH="$ENV/bin:$ENV/tools:/opt/subversion/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
-cd ~/lib
-for DYDIR in `echo "$DYLD_FALLBACK_LIBRARY_PATH" | sed 's/^\.://;s/:/ /g'`; do
-	for DYLIB in `find "$DYDIR" -name "*.dylib" -o -name "*.dylib.*" 2>/dev/null`; do
-		ln -sf "$DYLIB"
-	done
-done
-find . -xtype l | xargs rm 2>/dev/null
-cd
-
+dylink "$DYLD_FALLBACK_LIBRARY_PATH"
 archey
