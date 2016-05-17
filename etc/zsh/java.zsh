bab4d873794619edd8e7c0bfa0a6a4a08554b761
MoonFruit <dkmoonfruit@gmail.com>
2016-05-17 10:34:04 +0800

    bug fix

--- etc/zsh/java.zsh
+++ etc/zsh/java.zsh
@@ -2,3 +2,5 @@ export JAVA_HOME=`/usr/libexec/java_home`
 export JRE_HOME="$JAVA_HOME/jre"
 
 export JENKINS_URL="http://192.168.0.32/jenkins"
+
+alias ant="JAVA_HOME=`/usr/libexec/java_home -v1.7` ant"
--- etc/zsh/zlast.zsh
+++ etc/zsh/zlast.zsh
@@ -3,7 +3,8 @@ export APP_HOME="$WORKSPACE/gpp"
 export NODE_ID="YY"
 NOCD=1 source "$APP_HOME/etc/bashrc"
 
-source =(env | grep _HOME | sed 's/\(.*\)=.*/hash -d \1="$\1"/')
+# source =(env | grep _HOME | sed 's/\(.*\)=.*/hash -d \1="$\1"/')
+eval "$(env | grep _HOME | sed 's/\(.*\)=.*/hash -d \1="$\1"/')"
 
 # for PATH
 export PATH="$ENV/bin:$ENV/tools:/opt/subversion/bin:/usr/local/bin:/usr/local/sbin:$PATH"
