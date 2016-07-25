ad0eb5e391287db0acf70dd89c701dea9a575584
MoonFruit <dkmoonfruit@gmail.com>
2016-07-25 16:42:15 +0800

    add virtualenv support

---
+++ etc/zsh/themes/crack.zsh
@@ -0,0 +1,11 @@
+prompt_virtualenv() {
+	local virtualenv_path="$VIRTUAL_ENV"
+	if [[ -z "$virtualenv_path" ]]; then
+		virtualenv_path="system"
+	elif [[ "$virtualenv_path" = "/usr/local/var/python" ]]; then
+		virtualenv_path=""
+	fi
+	if [[ -n $virtualenv_path && -n $VIRTUAL_ENV_DISABLE_PROMPT ]]; then
+		prompt_segment blue black "(`basename $virtualenv_path`)"
+	fi
+}
--- etc/zsh/zlast.zsh
+++ etc/zsh/zlast.zsh
@@ -9,5 +9,9 @@ eval "$(env | grep _HOME | sed 's/\(.*\)=.*/hash -d \1="$\1"/')"
 # for PATH
 export PATH="$ENV/bin:$ENV/tools:/opt/subversion/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
+# virtualenv
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+source /usr/local/var/python/bin/activate
+
 # dylink "$DYLD_FALLBACK_LIBRARY_PATH"
 archey
