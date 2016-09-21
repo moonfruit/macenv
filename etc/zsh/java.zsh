67761d35906eccc405e914d92fc413d17fa59047
MoonFruit <dkmoonfruit@gmail.com>
2016-09-21 14:35:33 +0800

    bug fix

--- etc/zsh/java.zsh
+++ etc/zsh/java.zsh
@@ -1,6 +1,6 @@
 export JAVA_HOME=`/usr/libexec/java_home`
 export JRE_HOME="$JAVA_HOME/jre"
 
-export JENKINS_URL="http://192.168.0.32/jenkins"
+export JENKINS_URL="http://10.1.2.32/jenkins"
 
 alias ant="JAVA_HOME=`/usr/libexec/java_home -v1.7` ant"
