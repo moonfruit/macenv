e657479823a1afc991372c07f6077a990cb5f5a4
MoonFruit <dkmoonfruit@gmail.com>
2016-10-14 17:42:33 +0800

    add highlighting

--- etc/zsh/highlighting.zsh
+++ etc/zsh/highlighting.zsh
@@ -1,3 +1,4 @@
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 
 ZSH_HIGHLIGHT_STYLES[alias]='fg=green'
 ZSH_HIGHLIGHT_STYLES[builtin]='fg=yellow'
@@ -17,3 +18,12 @@ ZSH_HIGHLIGHT_STYLES[back-quoted-argument]='fg=magenta'
 ZSH_HIGHLIGHT_STYLES[redirection]='fg=white'
 
 ZSH_HIGHLIGHT_STYLES[comment]='fg=green,bold'
+
+ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=magenta'
+ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=cyan'
+ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=yellow'
+ZSH_HIGHLIGHT_STYLES[bracket-level-5]='fg=green'
+
+ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
+ZSH_HIGHLIGHT_PATTERNS+=('rm -fr *' 'fg=white,bold,bg=red')
