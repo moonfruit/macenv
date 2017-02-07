5d437260f2e3544d697153f3283bd76758909306
MoonFruit <dkmoonfruit@gmail.com>
2017-02-07 12:08:16 +0800

    bug fix

--- etc/zsh/zlast.zsh
+++ etc/zsh/zlast.zsh
@@ -7,13 +7,15 @@ NOCD=1 source "$APP_HOME/etc/bashrc"
 eval "$(env | grep _HOME | sed 's/\(.*\)=.*/hash -d \1="$\1"/')"
 
 # for PATH
-export PATH="$ENV/bin:$ENV/tools:/opt/subversion/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+USER_PYTHON_HOME="/Users/moon/Library/Python/2.7"
+export PATH="/opt/subversion/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="$ENV/bin:$ENV/tools:$USER_PYTHON_HOME/bin:$PATH"
 
 # virtualenv
-if [[ -n "$TERM" ]]; then
-	export VIRTUAL_ENV_DISABLE_PROMPT=1
-	source /usr/local/var/python/bin/activate
-fi
+# if [[ -n "$TERM" ]]; then
+	# export VIRTUAL_ENV_DISABLE_PROMPT=1
+	# source /usr/local/var//bin/activate
+# fi
 
 # catalog
 export XML_CATALOG_FILES="$ENV/package/vim/XMLCatalog/catalog.xml"
