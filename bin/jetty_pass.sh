67732a0f00569d76d882722a135d208517032010
MoonFruit <dkmoonfruit@gmail.com>
2017-03-31 10:33:28 +0800

    bug fix

--- bin/jetty_pass.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-JETTY_UTIL_LIB="/Users/moon/.m2/repository/org/eclipse/jetty/aggregate/jetty-all/9.2.5.v20141112/jetty-all-9.2.5.v20141112.jar"
-java -cp "$JETTY_UTIL_LIB" org.eclipse.jetty.util.security.Password $*
